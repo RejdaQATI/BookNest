4a52d83a38ed76f795bf066a41a4c3b0e073ef9c
RED <qati.rejda@gmail.com>
2025-03-03 16:29:36 +0100

    Book controller done

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['title', 'author_id', 'category_id', 'year', 'description'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
